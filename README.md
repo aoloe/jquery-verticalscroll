1912ca142e3204c3a967c8a1fb1b83b118b39b9f
ale rimoldi <ale@graphicslab.org>
2017-02-11 23:25:52 +0100

    create the plugin

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
-# jquery-verticalscroll
+# JQuery Vertical Scroll
 
-A small and simple JQuery plugin plugin for vertical scrolling a list of DIVs
+A small and simple JQuery plugin for vertical scrolling a list of DIVs.
+
+This plugin is based on this answer on Stackoverflow: <https://stackoverflow.com/a/13222226/5239250>
+
+## Features:
+
+- Multiple scrolling DIVs on a page.
+- Stops on mouseover.
+- Elements can have different height.
+
+## Usage
+
+- Include JQuery and the verticalScroll plugin in your html file.
+- Create a DIV of DIVs:
+
+  ~~~.html
+  <div class="verticalScroll">
+    <div>1 Lorem ipsum dolor sit</div>
+    <div>2 Lorem ipsum dolor sit</div>
+    <div>3 Lorem ipsum dolor sit</div>
+    <div>4 Lorem ipsum dolor sit</div>
+  </div>
+  ~~~
+- Define the outer DIV as a fixed sized box around the inner DIVs:
+
+  ~~~.css
+  .verticalScroll {
+    position: relative;
+    height: 180px;
+    overflow: hidden;
+  }
+
+  .verticalScroll > div {
+    position:absolute;
+  }
+  ~~~
+
+- attach the plugin to the DIV:
+
+  ~~~.js
+  $(document).ready(function() {
+      $(".verticalScroll").verticalScroll();
+  });
+  ~~~
+
+## Alternatives
+
+If you need more features, there are better, full featured, JQuery plugins:
+
+- <http://risq.github.io/jquery-advanced-news-ticker/index.html>
---
+++ jquery.verticalscroll.js
@@ -0,0 +1,76 @@
+/*!
+ * Scroll a list of DIVs inside of a DIV.
+ * https://github.com/aoloe/jquery-verticalscroll
+ *
+ * Copyright Ale Rimoldi
+ * Released under the MIT license.
+ *
+ * Date: 11.2.2017
+ */
+
+(function ($) {
+
+    $.fn.verticalScroll = function() {
+        function scroll($elem) {
+          var padding = 10;
+
+          if (!$elem.data("running")) {
+            setTimeout(function() {
+              scroll($elem)
+            }, 2400);
+            return;
+          }
+
+          //  create a sorted list of sub divs
+          var items = [];
+          $('div', $elem).each(function(j, divdiv) {
+            // if it's above the top, move the item to the bottom
+            var top = parseInt($(divdiv).css("top"));
+            if(top < 0) {
+                top = $elem.data("innerHeight") - $(divdiv).height() - padding;
+                $(divdiv).css("top", top);
+            }
+            items.push(divdiv);
+          });
+          items.sort(function(a, b) {return $(a).css("top") < $(b).css("top");});
+
+          // shift each div by the height of the first (at the css top) item
+          shift = parseInt($(items[0]).css("height"));
+          $(items).each(function(key, divdiv) {
+            var top = parseInt($(divdiv).css("top"));
+            $(divdiv).animate({ top: (parseInt(top) - shift - padding) }, 25 * shift);
+          });
+
+          // when all animations are done, start the next round
+          $('div', $elem).promise().done(function() {
+            scroll($elem);
+          });
+        }
+
+        $(this).each(function (i, div) {
+          var padding = 10;
+          var innerHeight = padding / 2;
+          $(div).data("running", true);
+          // TODO: maybe try to work on a fixed list of items instead of querying the dom
+          // console.log('items', window.verticalScrol.items);
+          // window.verticalScroll.items = [];
+          $('div', div).each(function(j, divdiv) {
+            $(this).css("top", innerHeight);
+            innerHeight += $(this).height() + padding;
+          });
+          $(div).data("innerHeight", innerHeight);
+          scroll($(this));
+
+          $(div).mouseenter(function($elem){
+              $($elem.currentTarget).data("running", false);
+          });
+              
+          $(div).mouseleave(function($elem){
+              $($elem.currentTarget).data("running", true);
+          });
+        });
+
+        return this;
+    };
+
+}(jQuery));
